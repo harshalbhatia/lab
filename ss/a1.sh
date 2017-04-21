d701bfac5dd0f67de2b0af806f2524885c5401b1
Harshal Bhatia <harshalbhatia@users.noreply.github.com>
2017-04-21 23:03:43 +0530

    Added ss-a1

---
+++ ss/a1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [ $# -gt 0 ]
+then
+	echo "Backed-up files are:"
+	tar -czvf a1_backup.tar $1
+else
+	echo "Please enter an argument."
+fi
+
+# Say 'a1.txt' is the file to be backed up.
+# Execution:
+# 	$ sh a1.sh a1.txt
+# 	Backed-up files are:
+# 	a1.txt
