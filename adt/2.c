f8cd3ccfe1f25236335d91ed12f5d7eb2b21bf35
Harshal Bhatia <harshalbhatia@users.noreply.github.com>
2017-04-25 22:08:33 +0530

    Init adt/2

---
+++ adt/2.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+int partition (int a[], int low, int high)
+{
+	int temp, key, i, j;
+	key = a[low];
+	i = low;
+	j = high + 1;
+	while (i <= j)
+	{
+		do {i += 1;} while (key >= a[i]);
+		do {j += 1;} while (key < a[j]);
+		if (i < j)
+		{
+			temp = a[i];
+			a[i] = a[j];
+			a[j] = temp;
+		}
+	}
+	temp = a[low];
+	a[low] = a[j];
+	a[j] = temp;
+	return j;
+}
+
+void quicksort(int a[], int low, int high)
+{
+	int mid;
+	if (low < high)
+	{
+		mid = partition(a, low, high);
+		quicksort(a, low, mid-1);
+		quicksort(a, mid+1, high);
+	}
+}
+
+int main()
+{
+	int a[10000], n, i, mid, low, high;
+	float res;
+	clock_t end, start;
+	//clrscr();
+	low = 0;
+	high = n - 1;
+	printf("Enter the size of an array.\n");
+	scanf("%d", &n);
+	for (i = 0; i < n; i++)
+	{
+		a[i] = rand();
+	}
+	printf("The unsorted array is:\n");
+	for (i = 0; i < n; i++)
+	{
+		printf("%d\n", a[i]);
+	}
+	start = clock();
+	quicksort(a, low, high);
+	end = clock();
+	printf("The sorted array is:\n");
+	for (i = 0; i < n; i++)
+	{
+		printf("%d\n", a[i]);
+	}
+	// res = (end - start)/CLK_TCK;
+	res = (end - start)/CLOCKS_PER_SEC;
+	printf("The time taken to sort %d elements is %f seconds.\n", n, res);
+	return 0;
+}
